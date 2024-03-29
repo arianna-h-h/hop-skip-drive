522665a78885143c04b097872d82481a75dfa04e
arianna haradon <ariannaharadon@gmail.com>
2024-03-29 17:48:06 -0400

    add basic controller, route, and db tables

---
+++ app/controllers/rides_controller.rb
@@ -0,0 +1,7 @@
+# app/controllers/rides_controller.rb
+
+class RidesController < ApplicationController
+  def index
+    # Your code to fetch and paginate rides in descending score order for a given driver
+  end
+end
