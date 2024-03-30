e4241beb3c404cde67bf17e1a5ee308f5da84a79
arianna haradon <ariannaharadon@gmail.com>
2024-03-30 16:44:41 -0400

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,11 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# Create drivers
+driver1 = Driver.create(home_address: '123 Main St')
+driver2 = Driver.create(home_address: '456 Elm St')
+driver3 = Driver.create(home_address: '789 Oak St')
+
+# Create rides
+Ride.create(start_address: '789 Oak St', destination_address: '101 Pine St', driver: driver1)
+Ride.create(start_address: '111 Maple St', destination_address: '222 Birch St', driver: driver2)
+Ride.create(start_address: '333 Walnut St', destination_address: '444 Cedar St', driver: driver1)
+Ride.create(start_address: '555 Elm St', destination_address: '666 Oak St', driver: driver2)
+Ride.create(start_address: '777 Pine St', destination_address: '888 Maple St', driver: driver1)
