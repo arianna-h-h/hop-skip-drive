ae13ce6faa05ce6692f542a01c8967edeefcade9
arianna haradon <ariannaharadon@gmail.com>
2024-04-01 18:11:47 -0400

    remove puts

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -1,10 +1,8 @@
 class RidesController < ApplicationController
   def index
-    puts params
     driver = Driver.find(params[:driver_id])
     ride_score_calculator = RideScoreCalculator.new
     rides_with_scores = []
-      puts "rides #{driver.rides.inspect}"
 
     driver.rides.each do |ride|
       ride_score = ride_score_calculator.calculate(ride.start_address, ride.destination_address, driver.home_address)
@@ -12,13 +10,10 @@ class RidesController < ApplicationController
         ride_id: ride.id,
         ride_score: ride_score
       }
-      puts "ride data #{ride_data}"
       rides_with_scores << ride_data
     end
     sorted_rides = rides_with_scores.sort_by { |ride_data| ride_data[:ride_score] }.reverse
-    puts "rides data #{sorted_rides}"
-  
-    render json: {rides_data: sorted_rides }
+    render json: { rides_data: sorted_rides }
   rescue ActiveRecord::RecordNotFound
     render json: { error: 'Driver not found' }, status: :not_found
   rescue StandardError => e
