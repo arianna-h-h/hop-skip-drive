cc39ebd8e5720209a79a91e94e9d396efa7f4b23
arianna haradon <ariannaharadon@gmail.com>
2024-04-01 16:08:05 -0400

    change access method in calculate

--- app/services/ride_score_calculator.rb
+++ app/services/ride_score_calculator.rb
@@ -6,9 +6,9 @@ class RideScoreCalculator
   def calculate(start_address, destination_address, driver_home_address)
     ride_stats = @google_maps_api.fetch_ride_stats(start_address, destination_address)
     commute_stats = @google_maps_api.fetch_ride_stats(driver_home_address, start_address)
-    ride_duration = ride_stats.duration
-    ride_distance = ride_stats.distance 
-    commute_duration = commute_stats.duration
+    ride_duration = ride_stats[:duration]
+    ride_distance = ride_stats[:distance]
+    commute_duration = commute_stats[:duration]
     # Calculate ride earnings
     earnings = 12 + (ride_distance > 5 ? (ride_distance - 5) * 1.50 : 0) + (ride_duration > 15 ? (ride_duration - 15) * 0.70 : 0)
 
