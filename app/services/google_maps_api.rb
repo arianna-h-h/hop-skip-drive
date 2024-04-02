b3b385b18fffe950ab30a135a0a907b0de2e20e9
arianna haradon <ariannaharadon@gmail.com>
2024-04-02 16:16:13 -0400

    modularize maps api service

--- app/services/google_maps_api.rb
+++ app/services/google_maps_api.rb
@@ -3,12 +3,24 @@ require 'json'
 
 class GoogleMapsApi
   def fetch_ride_stats(origin, destination)
-    origin_params = URI.encode_www_form({origins: origin})
-    destination_params = URI.encode_www_form({destinations: destination})
+    url = build_url(origin, destination)
+    response = Net::HTTP.get_response(URI(url))
+    handle_response(response)
+  rescue => e
+    puts "Error fetching maps data: #{e.message}"
+  end
+
+  private
+
+  def build_url(origin, destination)
     api_key = ENV.fetch('MAPS_API_KEY')
     base_url = ENV.fetch('MAPS_BASE_URL')
-    url = "#{base_url}#{origin_params}&#{destination_params}&units=imperial&key=#{api_key}"
-    response = Net::HTTP.get_response(URI(url))
+    origin_params = URI.encode_www_form({ origins: origin })
+    destination_params = URI.encode_www_form({ destinations: destination })
+    "#{base_url}#{origin_params}&#{destination_params}&units=imperial&key=#{api_key}"
+  end
+
+  def handle_response(response)
     if response.is_a?(Net::HTTPSuccess)
       parsed_response = JSON.parse(response.body)
       extract_distance_and_duration(parsed_response)
@@ -16,11 +28,7 @@ class GoogleMapsApi
       puts "API request failed with status code: #{response.code}"
       nil
     end
-  rescue => e
-    puts "Error fetching maps data: #{e.message}"
   end
-
-  private
 
   def extract_distance_and_duration(response)
     first_element = response["rows"][0]["elements"][0]
